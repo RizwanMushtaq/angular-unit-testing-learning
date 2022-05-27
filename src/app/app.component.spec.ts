5aced7ec47a9fe3b1fc01deda6375b435d8a991d
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-05-27 21:03:44 +0200

    unit testing in app-component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('unit-testing-project app is running!');
+    expect(compiled.querySelector('.title')?.textContent).toContain('unit-testing-project app is running!');
   });
 });
